df97be678fe9ae88010c540411a0cc772d832b27
Ma, Jiangnan <jiangnan.ma@intel.com>
2022-08-11 04:12:59 +0800

    first op

---
+++ csrc/lltm.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <torch/extension.h>
+
+torch::Tensor my_add(torch::Tensor a, torch::Tensor b) {
+  auto c = a + b;
+  return c;
+}
+
+std::vector<torch::Tensor> lltm_forward(
+    torch::Tensor input,
+    torch::Tensor weights,
+    torch::Tensor bias,
+    torch::Tensor old_h,
+    torch::Tensor old_cell) {
+  auto X = torch::cat({old_h, input}, /*dim=*/1);
+
+  auto gate_weights = torch::addmm(bias, X, weights.transpose(0, 1));
+  auto gates = gate_weights.chunk(3, /*dim=*/1);
+
+  auto input_gate = torch::sigmoid(gates[0]);
+  auto output_gate = torch::sigmoid(gates[1]);
+  auto candidate_cell = torch::elu(gates[2], /*alpha=*/1.0);
+
+  auto new_cell = old_cell + candidate_cell * input_gate;
+  auto new_h = torch::tanh(new_cell) * output_gate;
+
+  return {new_h,
+          new_cell,
+          input_gate,
+          output_gate,
+          candidate_cell,
+          X,
+          gate_weights};
+}
+
+torch::Tensor d_tanh(torch::Tensor z) {
+  return 1 - z.tanh().pow(2);
+}
+
+torch::Tensor d_elu(torch::Tensor z, torch::Scalar alpha = 1.0) {
+  auto e = z.exp();
+  auto mask = (alpha * (e - 1)) < 0;
+  return (z > 0).type_as(z) + mask.type_as(z) * (alpha * e);
+}
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+from torch.utils import cpp_extension
+
+setup(
+    name='lltm_cpp',						# 编译后的链接库名称
+    ext_modules=[
+        cpp_extension.CppExtension(
+            'lltm_cpp', ['csrc/lltm.cpp']		       # 待编译文件，及编译函数
+        )
+    ],
+    cmdclass={						       # 执行编译命令设置
+        'build_ext': cpp_extension.BuildExtension
+    }
+)
