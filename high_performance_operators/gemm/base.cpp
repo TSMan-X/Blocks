087bb3c847cd3d1e75c50633b287b129ee3dabe6
Ma, Jiangnan <jiangnan.ma@intel.com>
2023-09-19 08:13:32 +0800

    update

--- high_performance_operators/gemm/base.cpp
+++ high_performance_operators/gemm/base.cpp
@@ -11,11 +11,11 @@ struct matrix_2d {
         w = data[0].size();
     }
 
-    vector<type> & operator[](int i) {
+    std::vector<type> & operator[](int i) {
         return data[i];
     }
 
-    friend std::ostream & operator<<(std::ostream & o, const matrix_2d<type> m) {
+    friend std::ostream & operator<<(std::ostream & o, matrix_2d<type> & m) {
         for (int i = 0; i < m.h; ++i) {
             for (int j = 0; j < m.w; ++j) o << m[i][j] << " ";
             o << std::endl;
@@ -30,12 +30,29 @@ struct matrix_2d {
 
 template<class type>
 matrix_2d<type> & gemm2d(matrix_2d<type> & a, matrix_2d<type> & b, matrix_2d<type> & result) {
+    assert(a.w == b.h);
+    assert(result.w == a.w && result.h == b.h);
 
+    for (int i = 0; i < result.h; ++i) {
+        for (int j = 0; j < result.w; ++j) {
+            type tmp = 0;
+            for (int k = 0; k < a.w; ++k) {
+                tmp += a[i][k] * b[k][j];
+            }
+            result[i][j] = tmp;
+        }
+    }
+    return result;
 };
 
 int main() {
     std::vector<std::vector<float>> tmp(2, std::vector<float>{1, 1});
     auto a = matrix_2d<float>(tmp);
-    std::cout << a.h << std::endl;
+    auto b = a;
+    auto c = a;
+
+    gemm2d<float>(a, b, c);
+    std::cout << a << b << c << std::endl;
+
     return 0;
 }
